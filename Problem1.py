81806258a2a3f7fb30d2e7aa42c890284c753aec
Ashutosh5370 <patilashutsoh134@gmail.com>
2019-03-22 17:15:30 +0530

    Some random changes

--- Problem1.py
+++ Problem1.py
@@ -16,4 +16,7 @@ print(sum_exist([10,15,3,7] , 17))
 
 #For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-#Bonus: Can you do this in one pass?
+#Bonus: Can you do this in one pass
+
+
+#simple change
