a54392c5df807ec64fefc385e5cf590ebfc53469
Ashutosh patil <31342360+Ashutosh5370@users.noreply.github.com>
2019-03-24 12:14:31 +0530

    Create Problem8.py

---
+++ Problem8.py
@@ -0,0 +1,49 @@
+
+#problem Link =>https://www.hackerrank.com/challenges/angry-professor/problem
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the angryProfessor function below.
+def angryProfessor(k, a):
+
+    stu_On_time = 0
+
+    for i in a:
+        if(i <= 0):
+            stu_On_time += 1
+
+    if(stu_On_time >= k):
+        return "NO"
+    else:
+        return "YES"
+
+
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
