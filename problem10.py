0e15aea8dda7ebd2d48d75cb433845d0bf0fd53c
Ashutosh patil <31342360+Ashutosh5370@users.noreply.github.com>
2019-03-25 11:46:26 +0530

    Create problem10.py

---
+++ problem10.py
@@ -0,0 +1,58 @@
+#Problem link => https://www.hackerrank.com/challenges/electronics-shop/problem
+
+
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    temp = 0
+
+    for i in keyboards:
+        for j in drives:
+
+            k = i+ j
+
+            if( k<=b and temp < k):
+                temp = k
+                
+        
+                
+    if(temp> 0):
+        return temp
+    else:
+        return -1
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
